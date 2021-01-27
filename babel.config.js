6efce9335aa809ed2ee1d0e906cdd53524a7ef8d
Puneet Tiwari <puneettiwari61@gmail.com>
2021-01-27 18:28:59 +0530

    Set up react environment

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports = function(api) {
-  var validEnv = ['development', 'test', 'production']
-  var currentEnv = api.env()
-  var isDevelopmentEnv = api.env('development')
-  var isProductionEnv = api.env('production')
-  var isTestEnv = api.env('test')
+  var validEnv = ['development', 'test', 'production'];
+  var currentEnv = api.env();
+  var isDevelopmentEnv = api.env('development');
+  var isProductionEnv = api.env('production');
+  var isTestEnv = api.env('test');
 
   if (!validEnv.includes(currentEnv)) {
     throw new Error(
@@ -12,7 +12,7 @@ module.exports = function(api) {
         '"test", and "production". Instead, received: ' +
         JSON.stringify(currentEnv) +
         '.'
-    )
+    );
   }
 
   return {
@@ -37,6 +37,7 @@ module.exports = function(api) {
       ]
     ].filter(Boolean),
     plugins: [
+      "js-logger",
       'babel-plugin-macros',
       '@babel/plugin-syntax-dynamic-import',
       isTestEnv && 'babel-plugin-dynamic-import-node',
@@ -66,5 +67,5 @@ module.exports = function(api) {
         }
       ]
     ].filter(Boolean)
-  }
-}
+  };
+};
