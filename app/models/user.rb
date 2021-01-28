759480d340441ca062c518d49cde93aa82609018
Puneet Tiwari <puneettiwari61@gmail.com>
2021-01-28 18:22:23 +0530

    Added has_secure_password and validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 35 }
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+
   has_many :tasks, dependent: :destroy
+  has_secure_password
+
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :email, presence: true,
+                    uniqueness: true,
+                    length: { maximum: 50 },
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, confirmation: true, length: { maximum: 6 }
+  validates :password_confirmation, presence: true, on: :create
+
+  before_save :to_lowercase
+
+  private
+
+  def to_lowercase
+    email.downcase!
+  end
 end
