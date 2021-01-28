64e0001959bf51a249e628928aee6c9d40fed7a1
Puneet Tiwari <puneettiwari61@gmail.com>
2021-01-29 01:34:40 +0530

    Added login feature

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { either, isEmpty, isNil } from "ramda";
 import Logger from "js-logger";
+
 import { initializeLogger } from "common/logger";
 import Dashboard from "./components/Dashboard";
 import CreateTask from "./components/Tasks/CreateTask";
@@ -9,9 +11,15 @@ import { ToastContainer } from "react-toastify";
 import ShowTask from "./components/Tasks/ShowTask";
 import EditTask from "./components/Tasks/EditTask";
 import Signup from "./components/Authentication/Signup";
+import { getFromLocalStorage } from "helpers/storage";
+import PageLoader from "./components/PageLoader";
+import PrivateRoute from "./components/Common/PrivateRoute";
+import Login from "./components/Authentication/Login";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const authToken = getFromLocalStorage("authToken");
+  const isLoggedIn = !either(isNil, isEmpty)(authToken) && authToken !== "null";
 
   useEffect(() => {
     /*eslint no-undef: "off"*/
@@ -21,17 +29,30 @@ const App = () => {
     setAuthHeaders(setLoading);
   }, []);
 
+  if (loading) {
+    return (
+      <div className="h-screen">
+        <PageLoader />
+      </div>
+    );
+  }
+
   return (
     <Router>
       <ToastContainer />
       <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
         <Route exact path="/about" render={() => <div>About</div>} />
         <Route exact path="/tasks/create" component={CreateTask} />
         <Route exact path="/tasks/:id/edit" component={EditTask} />
         <Route exact path="/tasks/:id/show" component={ShowTask} />
-        <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute
+          path="/"
+          redirectRoute="/login"
+          condition={isLoggedIn}
+          component={Dashboard}
+        />
       </Switch>
     </Router>
   );
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,14 @@ class User < ApplicationRecord
 
   has_many :tasks, dependent: :destroy
   has_secure_password
+  has_secure_token :authentication_token
 
   validates :name, presence: true, length: { maximum: 35 }
   validates :email, presence: true,
                     uniqueness: true,
                     length: { maximum: 50 },
                     format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, confirmation: true, length: { maximum: 6 }
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true, on: :create
 
   before_save :to_lowercase
